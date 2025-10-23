aa60ecb43619588d162431b956871933d80d6e78
Subhranshu-Lenka <s.lenka.official2022@gmail.com>
2025-10-23 16:28:44 +0530

    q5

--- FileSystem/question_05/Solution.java
+++ FileSystem/question_05/Solution.java
@@ -1,7 +1,33 @@
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the file path. Select the source file.");
+        String inputPath = sc.nextLine().trim();
+
+        System.out.println("Enter the file path. Select the destination file.");
+        String outputPath = sc.nextLine().trim();
+
+        try{
+            FileInputStream in = new FileInputStream(inputPath);
+            FileOutputStream out = new FileOutputStream(outputPath);
+            int c;
+            while ((c = in.read()) != -1) {
+                out.write(c);
+            }
+            in.close();
+            out.close();
+            System.out.println("File copied successfully.");
+        }
+        catch(Exception err){
+            System.out.println("Found errors with the provided file-path.");
+            System.out.println(err);
+        }
+        finally{
+            System.out.println("Ending the Program.");
+        }
     }
 }
